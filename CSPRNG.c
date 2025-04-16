f6472d29d2aedf93a6e9174f88543351826c3e99
Ganesh Rayavarapu <116599817+gan0412@users.noreply.github.com>
2025-04-16 10:14:25 -0400

    starting generator

---
+++ CSPRNG.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(){
+    FILE* fileptr;
+    fileptr = fopen("input.txt", "wb");
+
+    if (fileptr==NULL){
+        perror("Error opening file!");
+        exit(1);
+    }
+
+    for (int i = 0; i<100; i++){
+        for (int j = 0; j<100; j++){
+            fprintf(fileptr, "%d ", rand()%100 + 1);
+        }
+        fprintf(fileptr, "\n");
+    }
+    fclose(fileptr);
+
+}
+
---
+++ ThePicker.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//picks number from each line in increasing index, store each one in buffer
+
+int main(){
+    FILE* fileptr;
+    fileptr = fopen("input.txt", "r");
+    //get index
+
+    //read number of index in input using fileptr
+
+    //add to buffer
+
+    //go to next line 
+
+    //repeat again (100 times)
+}
