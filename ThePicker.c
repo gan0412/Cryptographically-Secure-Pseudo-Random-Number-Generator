ad191695682d43f7e4bae5b4aab92f62df040751
Ganesh Rayavarapu <116599817+gan0412@users.noreply.github.com>
2025-04-18 13:02:22 -0400

    reading file to output

--- ThePicker.c
+++ ThePicker.c
@@ -6,9 +6,29 @@
 int main(){
     FILE* fileptr;
     fileptr = fopen("input.txt", "r");
+    int buffer[100]; 
+
+
     //get index
 
+
     //read number of index in input using fileptr
+    for (int i = 0; i<100; i++){
+        //pick random index from 1-99
+        int index = rand()%100;
+        char* line = NULL;
+        size_t len = 0;
+        ssize_t read;
+
+        //keep reading number until reaching index in line
+        for (int j = 0; j<index; j++){
+            fscanf(fileptr, "%s");
+        }
+        //read number of index
+        //store in buffer[i]
+        //move fileptr to next line 
+
+    }
 
     //add to buffer
 
