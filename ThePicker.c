c2292beab3938aad4bfac44da6299b1b372a92f3
Ganesh Rayavarapu <116599817+gan0412@users.noreply.github.com>
2025-04-20 17:19:20 -0400

    add output

--- ThePicker.c
+++ ThePicker.c
@@ -4,30 +4,56 @@
 //picks number from each line in increasing index, store each one in buffer
 
 int main(){
+    //open input file, set up buffer
     FILE* fileptr;
     fileptr = fopen("input.txt", "r");
     int buffer[100]; 
 
+    if (fileptr==NULL){
+        perror("Error opening input file");
+        return 1;
+    }
+
+
+    //open output file
+    FILE* output;
+    output = fopen("output.txt", "w");
+
 
     //get index
-
+    // int* bufpoint = buffer;
 
     //read number of index in input using fileptr
     for (int i = 0; i<100; i++){
         //pick random index from 1-99
         int index = rand()%100;
-        char* line = NULL;
-        size_t len = 0;
-        ssize_t read;
+        // printf("%d\n", index);
 
+        int currind = 0;
+        char ch;
         //keep reading number until reaching index in line
-        for (int j = 0; j<index; j++){
-            fscanf(fileptr, "%s");
+        while (fscanf(fileptr, "%c", &ch) == 1)
+        {
+            //everytime currind = index, write the read character to output 
+            //everytime a space is encountered, add one to currind 
+            
+            // printf("%d\n", ch);
+            if (currind==index){
+                // *bufpoint = currint;
+                fputc(ch, output);
+            }
+            
+            if (ch==' '){
+                currind++;
+            }
+            
         }
+        // bufpoint++;
+
         //read number of index
         //store in buffer[i]
         //move fileptr to next line 
-
+        
     }
 
     //add to buffer
@@ -35,4 +61,24 @@ int main(){
     //go to next line 
 
     //repeat again (100 times)
+    fclose(fileptr);
+    fclose(output);
+
+    // for (int k=0; k<100; k++){
+    //     printf("%d ", buffer[k]);
+    // }
+    // FILE* output;
+    // output = fopen("output.txt", "w");
+
+    // if (output==NULL){
+    //     perror("Error opening output file");
+    //     return 1;
+    // }
+
+    // for (int k=0; k<100; k++){
+    //     fprintf(output, "%d ", buffer[k]);
+    // }
+
+    // fclose(output);
+    return 0;
 }
