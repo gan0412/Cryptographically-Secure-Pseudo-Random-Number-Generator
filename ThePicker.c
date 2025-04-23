a335cc4ef853f01b775e10e074650cb3113aa2eb
Ganesh Rayavarapu <116599817+gan0412@users.noreply.github.com>
2025-04-23 13:12:26 -0400

    in process of adding buffer

--- ThePicker.c
+++ ThePicker.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //picks number from each line in increasing index, store each one in buffer
 
@@ -7,7 +8,10 @@ int main(){
     //open input file, set up buffer
     FILE* fileptr;
     fileptr = fopen("input.txt", "r");
-    int buffer[100]; 
+    char line[400];
+    int array[100]; 
+
+    const char delimeter = ' ';
 
     if (fileptr==NULL){
         perror("Error opening input file");
@@ -27,22 +31,51 @@ int main(){
         int currind = 0;
         char ch;
 
-        //keep reading every number until new line (end of line) is reached 
-        fscanf(fileptr, "%c", &ch);
-        while (ch != '\n')
-        {
-            //write to output when number with chosen index is reached and it is not space 
-            if (currind==index & ch!=' '){
-                fputc(ch, output);
-            }
-            
-            //when space is reached, new number or index has started 
-            if (ch==' '){
-                currind++;
-            }
-            fscanf(fileptr, "%c", &ch);
-            
+        //just read the whole line
+        //store it in the line
+        fgets(line, sizeof(line), fileptr);
+        int i = 0;
+
+        // while (line[i]!='\n' & i<400){
+        //     printf("%c", line[i]);
+        //     fflush(stdout);
+        //     i++;
+        // }
+
+        //tokenize the line (it will give first pointer)
+        char* num = strtok(line, " ");
+        int currind = 0;
+
+        //keep incrementing pointer, until correct index is encoutnered 
+        //then just pick the chosen index
+        while (num != NULL & (currind != index)){
+            // printf("%s ", num);
+            num = strtok(NULL, " ");
+            currind++;
         }
+
+        //write that element to output file 
+        fputc(num, output);
+        //go to next line
+
+        
+        
+        //keep reading every number until new line (end of line) is reached 
+        // fscanf(fileptr, "%c", &ch);
+        // while (ch != '\n')
+        // {
+        //     //write to output when number with chosen index is reached and it is not space 
+        //     if (currind==index & ch!=' '){
+        //         fputc(ch, output);
+        //     }
+            
+        //     //when space is reached, new number or index has started 
+        //     if (ch==' '){
+        //         currind++;
+        //     }
+        //     fscanf(fileptr, "%c", &ch);
+            
+        // }
 
         
     }
